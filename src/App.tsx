20f6b53872e35e428492ddb56edc07e977a75b75
Sampo Savolainen <sampo.savolainen@spatineo.com>
2024-02-26 08:13:24 +0000

    component structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Hello } from './components/Hello'
+import { Hello } from './components/hello/Hello'
 
 function App() {
   const [count, setCount] = useState(0)
--- stories/components/Hello.stories.ts
+++ stories/components/Hello.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Hello } from '../../src/components/Hello';
+import { Hello } from '../../src/components/hello/Hello';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
