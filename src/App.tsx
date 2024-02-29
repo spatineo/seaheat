87d6833a1d27685185be1ce33cdf6b24d754e42e
stanleyfestus <festusstanley@gmail.com>
2024-02-29 12:17:42 +0000

    Removed graphView from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import './App.css'
-import { Hello } from './components/hello/Hello';
 import { GraphView } from './components/GraphView/GraphView';
-import { TemperatureComponent } from './components/TemperatureComponent/TemperatureComponent';
-import { ComponentInfo } from './components/ComponentInfo/ComponentInfo';
-import Ecahrts from './components/Echarts/EchartsComponent';
-import { data } from './data';
 
 function App() {
  
@@ -12,11 +7,8 @@ function App() {
     <>
       <h1>SeaHeat placeholder</h1>
       <div className="card">
-        <Hello label="hello" />
         <GraphView>
-          <ComponentInfo position={data.position} depth={- 25} name="Salmisaari - intake 1" meters="m"/>
-          <Ecahrts option={data.option} height={300} />
-          <TemperatureComponent tempatureValues={data.arg} />
+         
         </GraphView>
       </div>
     </>
