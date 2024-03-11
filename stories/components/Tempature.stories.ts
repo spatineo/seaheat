c83ae524a6581a1fce9d44044bb8cd6e80cd34cb
stanleyfestus <festusstanley@gmail.com>
2024-03-11 09:15:00 +0000

    Removed location

--- stories/components/Tempature.stories.ts
+++ stories/components/Tempature.stories.ts
@@ -18,20 +18,16 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
    ...data.one,
-   location: [65.893856, 24.051956]
   }
 };
 
 export const Large: Story = {
   args: {
     ...data.two,
-  location: [55.893856, 19.051956]
   }
 };
 
 export const Small: Story = {
   args:{ 
     ...data.three,
-    location: [60.893856, 21.051956],
-    legend: data.one.legend
 }}
