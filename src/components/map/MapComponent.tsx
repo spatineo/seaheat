2417c5fcedca674831ca9639aaf856ed3b4a3ebe
Sampo Savolainen <sampo.savolainen@spatineo.com>
2024-03-11 11:08:45 +0000

    wip: start work on map component

---
+++ src/components/map/MapComponent.tsx
@@ -0,0 +1,44 @@
+import { Box } from '@chakra-ui/react';
+import { useEffect, useRef, useState } from "react";
+
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import {OSM} from 'ol/source.js';
+import {Tile as TileLayer} from 'ol/layer.js';
+
+import MapContext from "./MapContext";
+
+import 'ol/ol.css';
+import './MapComponent.css';
+
+
+export const MapComponent = () => {
+    const mapRef = useRef(null);
+    const [map, setMap] = useState<Map | null>(null);
+
+    useEffect(() => {
+        if (!mapRef.current) return;
+
+        const mapObject: Map = new Map({
+            view: new View({
+                center: [2749287.033361, 8966980.662191],
+                zoom: 5,
+                projection: "EPSG:3857",
+            }),
+            layers: [new TileLayer({
+                source: new OSM()
+            })]
+        });
+        mapObject.setTarget(mapRef.current);
+
+        setMap(mapObject);
+
+        return () => mapObject.setTarget(undefined);
+    }, [mapRef])
+
+    return (
+        <MapContext.Provider value={{ map }}>
+            <Box ref={mapRef} className="map-component" />
+        </MapContext.Provider>
+    );
+}
---
+++ src/components/map/MapContext.tsx
@@ -0,0 +1,10 @@
+import { createContext } from 'react';
+import { Map } from 'ol';
+
+interface MapContextType {
+	map: Map | null;
+}
+
+const MapContext = createContext({} as MapContextType);
+
+export default MapContext;
