06f3c700d20acf5c8629fbef8e7cc80e50090a93
stanleyfestus <festusstanley@gmail.com>
2024-03-11 06:57:56 +0000

    Added legend

--- src/components/TemperatureComponent/TemperatureComponent.tsx
+++ src/components/TemperatureComponent/TemperatureComponent.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import {
   Box,
   Table,
@@ -8,9 +7,7 @@ import {
   Th,
   Td,
   TableContainer,
-  Input,
   Flex,
-  HStack
 } from '@chakra-ui/react'
 import './TemperatureComponent.css'
 
@@ -44,16 +41,6 @@ interface TemperatureProps {
 }
 
 export const TemperatureComponent = (options : TemperatureProps ) => {
-  const [ depth, setDepth ] = useState<number>(-31);
-  const [ name, setName ] = useState<string>('');
-
-  const handleDepthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseFloat(event.target.value);
-    value < options.seabedDepth ? setDepth(0) : setDepth(value);
-  };
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
 
   const table = () => {
     return (
@@ -61,26 +48,31 @@ export const TemperatureComponent = (options : TemperatureProps ) => {
             <Table className='temperature-component-table'>
               <Thead>
                 <Tr>
-                  {options.axes.x.values.map((value, index) => <Th key={index} m={8} p="4">{value}</Th>)}
+                  {options.axes.x.values.map((value, index) => <Th key={index}  m={8}>{value}</Th>)}
                 </Tr>
                 </Thead>
                   <Tbody>
-                  {options.axes.y.values.map((yValue, yIndex) =>
-                  <Tr key={yIndex} className='temperature-component-tr'>
+                  {options.axes.y.values.map((yValue, yIndex) => {
+                    const yValueNumber = (typeof yValue === "string") && yValue.replace(/\D/g, '')
+                   if(Number(yValueNumber) < 60){ return <Tr key={yIndex} className='temperature-component-tr'>
                       {options.axes.x.values.map((xValue, xIndex) => {
                         const cellValue = options.data.find(d => d.x === xValue && d.y === yValue)?.value;
-                        const bgColor = (cellValue !== undefined) && (cellValue < 0 ) ? 
-                          '#5624ac' : (cellValue !== undefined && cellValue >= 0 && cellValue <= 4) ? 
-                          '#435caf' : cellValue! >= 5 && cellValue! <= 10 ? '#8cd67a' : cellValue! >= 11 && cellValue! <= 15 ? 
-                          '#eed323' : cellValue! >= 16 && cellValue! <= 20 ? '#ca8c3b' : cellValue! >= 21 && cellValue! <= 25 ? '#be3e26' : '#949494';
-                        return <Td bgColor={bgColor} key={xIndex}>
-                        {typeof cellValue === "number" ? `\u00A0` : options.seabedDepth} 
+                        const legendItem = options.legend.find(l => l.minValue <= cellValue! && cellValue! <= l.maxValue);
+                        const bgColor = legendItem && legendItem.color;
+                        return <Td key={xIndex} className={'temperature-component-td'} bgColor={bgColor}>
+                        {typeof cellValue === "number" && `\u00A0`} 
                       </Td>
                       })
                       }
                       <Th>{yValue === "-10m" ? `\u00A0` : yValue}</Th>
                     </Tr>
-                    )
+                    } else {
+                      return <Tr className='temperature-component-tr' key={yIndex}>
+                      <Td colSpan={options.axes.x.values.length} bgColor="#949494" className='temperature-component-th'>{options.seabedDepth}</Td>
+                      <Th>{yValue}</Th>
+                    </Tr>
+                    }
+                    })
                   }
                   </Tbody>
             </Table>
@@ -88,51 +80,29 @@ export const TemperatureComponent = (options : TemperatureProps ) => {
     )
   }
 
-  const inputContent = () => {
+  const columnCount = Math.ceil(options.legend.length / 3);
+  const legends = () => {
     return (
-      <Box mb="8">
-        <Flex flexDirection="column">
-          <HStack mb="2" w="40%">
-            <Box flex="1">Location: </Box>
-            <Box flex="2">{`${options.location[0]}, ${options.location[1]}`}</Box>
-          </HStack>
-          <HStack w="38.5%" mb="2">
-            <Box flex="1" marginRight="2">Depth:</Box>
-            <Box flex="2">
-            <HStack>
-              <Input
-                type="number"
-                min={-100}
-                max={0}
-                value={depth}
-                onChange={handleDepthChange}
-                size='sm'
-                variant='flushed'
-                p="0"
-              /><Box>m</Box></HStack>
-            </Box>
-          </HStack>
-          <HStack w="38%" mb="2">
-            <Box mr="2" flex="1">Name:</Box>
-            <Box flex="2">
-              <Input
-                value={name}
-                onChange={handleNameChange}
-                size='sm'
-                variant='flushed'
-                />{' '} 
-            </Box>
-          </HStack>
-        </Flex>
-      </Box>
-    )
-  }
+    <Flex mt={8}>
+      {[...Array(columnCount)].map((_, columnIndex) => (
+        <Box key={columnIndex}>
+          {options.legend.slice(columnIndex * 3, (columnIndex + 1) * 3).map(({ color, minValue, maxValue }, index) => (
+            <Flex key={index} alignItems="center" mr={24}>
+              <Box mr={4} bgColor={color} minW="12" minH="10"></Box>
+              <Box>{maxValue < 0 ? '< 0' : `${minValue} - ${maxValue}`}</Box>
+            </Flex>
+          ))}
+        </Box>
+      ))}
+    </Flex>
+  )}
 
   return (
-    <Box border="1px solid grey" borderRadius="3" p="4">
-      <Box>{inputContent()}</Box>
-      <Box mb="4">Pipeline length to facility: - km</Box>
+    <Box p="4">
       <Box>{table()}</Box>
+      <Box>
+        {legends()}
+      </Box>
     </Box>
   );
 }
