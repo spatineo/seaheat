b1366b0227a84510735cf09d6751a8e5856ce643
Sampo Savolainen <sampo.savolainen@spatineo.com>
2024-03-12 07:18:47 +0000

    chore: move wrapAction to utils.ts

--- src/app/connected/ConnectedIntakeComponent.tsx
+++ src/app/connected/ConnectedIntakeComponent.tsx
@@ -2,21 +2,16 @@ import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../../store"
 import { IntakeComponent } from "../../components/intake/IntakeComponent"
 import { setName, setDepth, setLocation } from "../slices/intake"
+import { wrapAction } from "./utils"
 
 export const ConnectedIntakeComponent = () => {
     const intakeProps = useSelector((state: RootState) => state.intake)
     const dispatch = useDispatch();
 
     const intakeCallbacks = {
-        setName: (name : string | null) => {
-            dispatch(setName(name));
-        },
-        setDepth: (depth : number | null) => {
-            dispatch(setDepth(depth));
-        },
-        setLocation: (location : Array<number | null>) => {
-            dispatch(setLocation(location));
-        }
+        setName: wrapAction(setName, dispatch),
+        setDepth: wrapAction(setDepth, dispatch),
+        setLocation: wrapAction(setLocation, dispatch)
     }
 
     return (<IntakeComponent {...intakeProps} {...intakeCallbacks} /> )
