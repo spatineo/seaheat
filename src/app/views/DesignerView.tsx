bd6b910b5f3ece558e87fc270e7e4816dc8b525a
Sampo Savolainen <sampo.savolainen@spatineo.com>
2024-03-15 07:51:28 +0000

    chore: move import logic to custom hook

--- src/app/views/DesignerView.tsx
+++ src/app/views/DesignerView.tsx
@@ -1,41 +1,13 @@
-import { Box, Button, Heading, Input, useToast } from "@chakra-ui/react"
 import { useDispatch } from "react-redux";
-import { ExportFile, exportState, importState, validateImportFile } from "../../middleware/ImportExportMiddleware";
+import { Box, Button, Heading, Input } from "@chakra-ui/react"
+import { exportState } from "../../middleware/ImportExportMiddleware";
+import { useFileImport } from "../hooks/useFileImport";
 
 
 export const DesignerView = () => {
     const dispatch = useDispatch();
-    const toast = useToast();
-
-    const loadFileForImport = (file : File | null) => {
-        if (!file) return;
-
-        const fileReader = new FileReader();
-        fileReader.onloadend = () => {
-            if (fileReader.result === null) {
-                toast({
-                    title: 'Unable to read file',
-                    description: 'The browser did not read the file',
-                    status: 'error',
-                    isClosable: true
-                });
-                return;
-            }
-            try {
-                const storedState = JSON.parse(String(fileReader.result)) as ExportFile;
-                validateImportFile(storedState);
-                dispatch(importState(storedState));
-            } catch(e) {
-                toast({
-                    title: 'Unable to read file',
-                    description: 'Did you perhaps send the wrong type of file?',
-                    status: 'error',
-                    isClosable: true
-                });
-            }
-        }
-        fileReader.readAsText(file);
-    }
+    
+    const { loadFileForImport } = useFileImport();
 
     return (
         <Box>
