cc9fc864e0b71f7eac0a8afcc1f15f83fbe84dfe
stanleyfestus <festusstanley@gmail.com>
2024-03-27 10:33:06 +0200

    code clean up

--- stories/components/Tempature.stories.tsx
+++ stories/components/Tempature.stories.tsx
@@ -1,73 +1,56 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 import { TemperatureComponent } from "../../src/components/TemperatureComponent/TemperatureComponent";
-import { data } from './Temperature.data';
+import { data } from "./Temperature.data";
 import { Box, ChakraProvider } from "@chakra-ui/react";
-import React from 'react'
+import React from "react";
 
 const meta = {
-  title: 'Components/Temperature',
+  title: "Components/Temperature",
   component: TemperatureComponent,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   render: (args) => (
     <ChakraProvider>
       <Box>
         <TemperatureComponent {...args} />
       </Box>
-    </ChakraProvider> 
+    </ChakraProvider>
   ),
 } satisfies Meta<typeof TemperatureComponent>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-
 export const Primary: Story = {
   args: {
-   ...data.one,
-  }
+    ...data.one,
+  },
 };
-
 
 export const WideBox: Story = {
   args: {
-   ...data.one,
+    ...data.one,
   },
   render: (args) => (
     <ChakraProvider>
-      <Box width={'800px'}>
+      <Box width={"90%"}>
         <TemperatureComponent {...args} />
       </Box>
-    </ChakraProvider> 
+    </ChakraProvider>
   ),
 };
-
 
 export const NarrowBox: Story = {
   args: {
-   ...data.one,
+    ...data.one,
   },
   render: (args) => (
     <ChakraProvider>
-      <Box width={'300px'}>
+      <Box width={"80%"}>
         <TemperatureComponent {...args} />
       </Box>
-    </ChakraProvider> 
+    </ChakraProvider>
   ),
 };
-
-
-/*
-export const Large: Story = {
-  args: {
-    ...data.two,
-  }
-};
-
-export const Small: Story = {
-  args:{ 
-    ...data.three,
-}}
-*/
