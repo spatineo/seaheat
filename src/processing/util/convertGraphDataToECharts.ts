810081c50af83d3cadd61725ec0778a019ba41dd
stanleyfestus <festusstanley@gmail.com>
2024-04-05 12:11:55 +0000

    legend postion and titile

--- src/processing/util/convertGraphDataToECharts.ts
+++ src/processing/util/convertGraphDataToECharts.ts
@@ -2,8 +2,16 @@ import { GraphData } from "../../types"
 
 export const convertGraphDataToECharts = (data: GraphData) => {
   return {
+    title: {
+      text: data.unit,
+      right: 80,
+      top: 20
+    },
     legend: {
-      data: data.series.map(serie => serie.label)
+      data: data.series.map(serie => serie.label),
+      orient: "vertical" as const,
+      right: "0%",
+      top: 50
     },
     series: data.series.map(serie => ({
       name: serie.label,
