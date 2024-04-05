99b83380a709cfbb67566e4d3ddee44baab3dff4
stanleyfestus <festusstanley@gmail.com>
2024-04-05 09:30:07 +0000

    Refactored optionData

--- src/components/GraphComponent/GraphComponent.tsx
+++ src/components/GraphComponent/GraphComponent.tsx
@@ -3,11 +3,26 @@ import * as echarts from 'echarts';
 import { Box } from '@chakra-ui/react';
 import { EChartOption } from "echarts";
 
-interface GraphOptions {
-  option: EChartOption
+interface Axis <T>{
+  label: string,
+  values: T[];
 }
 
-export const GraphComponent = ({ option }: GraphOptions) => {
+export interface Series {
+  label: string,
+  values: Array<number>
+}
+interface GraphOptions {
+  optionData: {
+    unit: string,
+    axes: {
+        x: Axis<string>
+    },
+    series: Array<Series>
+  }
+}
+
+export const GraphComponent = ({ optionData }: GraphOptions) => {
   const graphRef = useRef<HTMLDivElement>(null)
   const [graph, setGraph] = useState<echarts.ECharts | null>(null);
 
@@ -19,7 +34,7 @@ export const GraphComponent = ({ option }: GraphOptions) => {
     const g = echarts.init(graphRef.current, null, {
       height: 300,
       renderer: 'canvas',
-      useDirtyRect: false
+      useDirtyRect: false,
     });
 
     if (!g.isDisposed()) {
@@ -43,15 +58,44 @@ export const GraphComponent = ({ option }: GraphOptions) => {
   }, []);
 
   useEffect(() => {
-    if (!graph) return;
-    if (option && typeof option === 'object') {
-      graph.setOption(option);
+    if (!graph || !optionData || !Array.isArray(optionData.series) || optionData.series.length === 0) {
+      return;
+  }
+    if ((optionData !== null && optionData !== undefined )&& (typeof optionData === 'object' && optionData.series !== undefined)) {
+      const options: EChartOption = {
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: optionData.series.map(serie => serie.label)
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: optionData.axes.x.values
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: optionData.series.map(serie => ({
+          name: serie.label,
+          type: 'line',
+          data: serie.values
+        }))
+      };
+      graph.setOption(options);
     }
   
-  }, [option, graph]);
+  }, [optionData, graph]);
 
   return (
-    <Box sx={{ minW: "100%", border: "1px solid black"}}>
+    <Box sx={{minW: "100%", border: "1px solid #DCDCDC", borderRadius: "6px"}}>
       <Box ref={graphRef}>
       </Box>
     </Box>
