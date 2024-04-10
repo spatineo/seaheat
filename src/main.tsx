5a3ab330890291628e6f1a96650051e58a255772
stanleyfestus <festusstanley@gmail.com>
2024-04-10 14:54:28 +0000

    Added theme chakraui

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme  } from "@chakra-ui/react";
 import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store';
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        margin: "0",
+        padding: "0",
+        boxSizing: "border-box",
+        maxHeight: "inherit",
+       
+      },
+      "#root": {
+        padding: "0",
+        margin: "0",
+        boxSizing: "border-box",
+        height: "100vh",
+        width: "100%",
+       
+      }
+    }
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </Provider>
