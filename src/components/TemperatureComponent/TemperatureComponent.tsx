8ecf0cc4c8afb7e8a3637a064d5b87ad55b7396e
stanleyfestus <festusstanley@gmail.com>
2024-04-12 06:34:30 +0000

    Legend display fixed to be robust

--- src/components/TemperatureComponent/TemperatureComponent.tsx
+++ src/components/TemperatureComponent/TemperatureComponent.tsx
@@ -151,46 +151,33 @@ export const TemperatureComponent = ({ data, height, marker }: TemperatureProps)
 
   const legendsContent = (
     <Flex boxSizing="border-box" w="100%" mt="4" flexWrap="wrap" gap="4">
-      {data.legend.map(({ color, minValue, maxValue }, index) => (
-        <Flex
-          key={index}
-          minW="20%"
-          boxSizing="border-box"
-          alignItems="center"
-          textAlign="center"
-        >
-          <Box bgColor={color} w="16%" h="60%" mr="1"></Box>
-          <Flex w="70%">
-            {maxValue < 0 ? (
-              <Flex flexWrap="nowrap" w="100%" alignItems="center">
-                <Box flex="30%">{`<`}</Box>
-                <Box flex="20%" mr="1%"></Box>
-                <Box flex="30%">{`0`}</Box>
-                <Box flex="19%">{"°C"}</Box>
-              </Flex>
-            ) : minValue >= 26 ? (
-              <Flex flexWrap="nowrap" w="100%" alignItems="center">
-                <Box flex="30%">{`>`}</Box>
-                <Box flex="20%" mr="1%"></Box>
-                <Box flex="30%">{`25`}</Box>
-                <Box flex="19%">{"°C"}</Box>
-              </Flex>
-            ) : (
+      {data.legend.map(({ color, minValue, maxValue }, index) => {
+        return (
+          <Flex
+            key={index}
+            minW="24%"
+            maxW="24%"
+            boxSizing="border-box"
+            alignItems="center"
+            textAlign="center"
+          >
+            <Box bgColor={color} w="16%" h="60%" mr="1"></Box>
+            <Flex w="70%">
               <Flex flexWrap="nowrap" w="100%" alignItems="center">
                 <Box flex="30%" mr="1%">{`${minValue}`}</Box>
                 <Box flex="20%" mr="1%">
-                  -
-                </Box>
+                →
+              </Box>
                 <Box flex="30%" mr="1%">{`${maxValue}`}</Box>
                 <Box flex="19%">{`°C`}</Box>
               </Flex>
-            )}
+            </Flex>
           </Flex>
-        </Flex>
-      ))}
+        );
+      })}
     </Flex>
   );
-
+  
   return (
     <Box p="4" boxSizing="border-box" style={{ position: "relative" }}>
       <Box>{tableContent}</Box>
