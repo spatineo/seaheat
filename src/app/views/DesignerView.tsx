39d8769fcacba22dce59463e4bd95e9be9512d73
stanleyfestus <festusstanley@gmail.com>
2024-04-22 04:21:36 +0000

    Edited css rules

--- src/app/views/DesignerView.tsx
+++ src/app/views/DesignerView.tsx
@@ -12,16 +12,24 @@ export const DesignerView = () => {
     const { loadFileForImport } = useFileImport();
 
     return (
-      <Box position="relative" pl="2" m="0.9">
-            <Heading  fontSize="calc(12px + 0.5vw + 0.5vh)">SeaHeat Designer</Heading>
-            <Flex alignItems="center"  p={0} >
-                <Button onClick={() => dispatch(exportState())} mr="2">Export</Button>
+      <Box position="relative" pl="2" m="0.9" boxSizing="border-box" w="100%">
+            <Heading  fontSize="calc(12px + 0.5vw + 0.5vh)" mb="1">SeaHeat Designer</Heading>
+            <Flex alignItems="center"  p={0} w="100%">
+                <Button 
+                    flex="25%" 
+                    onClick={() => dispatch(exportState())} 
+                    mr="2" fontWeight={400} 
+                    backgroundColor="#f9f9f9"
+                    fontSize="calc(4px + 0.5vw + 0.5vh)">Export</Button>
+                <Box w="75%">
                 <UploadView  
                         onChange={loadFileForImport} 
                         accept={'application/json'} 
                         buttonText="Import" 
                         dragDropText="Drag and Drop"
                     />
+                </Box>
+               
             </Flex>
         </Box>
     )
