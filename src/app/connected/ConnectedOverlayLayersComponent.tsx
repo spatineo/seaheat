7fa267e02c5d4a0e1de2c048f3ce2bec0e80f705
Sampo Savolainen <sampo.savolainen@spatineo.com>
2024-04-29 06:58:25 +0000

    refactor state a bit: move layerDimensions inside map in data slice

--- src/app/connected/ConnectedOverlayLayersComponent.tsx
+++ src/app/connected/ConnectedOverlayLayersComponent.tsx
@@ -7,7 +7,7 @@ import { WMSLayer } from "../../components/map/layer/WMSLayer"
 export const ConnectedOverlayLayersComponent = () => {
     const visibleLayers = useSelector((state: RootState) => state.uiState.map.visibleLayers)
     const layers = useSelector((state: RootState) => state.data.layers)
-    const layerDimensions = useSelector((state: RootState) => state.uiState.layerDimensions);
+    const layerDimensions = useSelector((state: RootState) => state.uiState.map.layerDimensions);
 
     return useMemo(() => visibleLayers.map((vl, idx) => {
         const al = availableLayers.find((al) => al.id === vl.id);
