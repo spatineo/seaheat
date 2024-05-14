45a847f379ab9a8ce0c3f688ec302f2d287fd949
stanleyfestus <festusstanley@gmail.com>
2024-05-14 11:15:09 +0000

    trim and regex seperated to own function

--- src/processing/util/helpers.ts
+++ src/processing/util/helpers.ts
@@ -5,15 +5,21 @@ function isValidDateString (dateString: string) {
   return isValid(parseISO(dateString))
 }
 
-export function parseDimensionValues (valueString: string) {
+export function convertDimensionStringValue (stringValue: string) {
   // eslint-disable-next-line no-useless-escape
-  const values = valueString.split(/[,\/]+/).filter(value => value.trim() !== '')
+  return stringValue.trim().split(/[,\/]+/)
+    .map(value => value.trim())
+    .filter(value => value !== '')
+}
+
+export function parseDimensionValues (valueString: string) {
+  const values = convertDimensionStringValue(valueString)
   const parsedValues: (string | Date)[] = []
   if (isValidDateString(values[0]) && isValidDateString(values[1])) {
+    const parsedDuration = duration.toDays(values[2])
     if (!isAfter(values[0], values[1])) {
       const startDate = new Date(values[0])
       const endDate = new Date(values[1])
-      const parsedDuration = duration.toDays(values[2])
       let currentDate: Date = new Date(startDate)
 
       while (currentDate <= endDate) {
