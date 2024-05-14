e65037c421ef23434a74c4cd100c22180b66d029
stanleyfestus <festusstanley@gmail.com>
2024-05-14 07:59:47 +0000

    helper function refactored

--- src/processing/util/helpers.ts
+++ src/processing/util/helpers.ts
@@ -1,30 +1,28 @@
-import { isBefore, parseISO, isValid, add, format } from "date-fns"
+import { isAfter, parseISO, isValid, add, format } from "date-fns"
+import * as duration from "duration-fns"
 
-function isValidDateString (dateString: string): boolean {
+function isValidDateString (dateString: string) {
   return isValid(parseISO(dateString))
 }
 
 export function parseDimensionValues (valueString: string) {
   // eslint-disable-next-line no-useless-escape
-  const values = valueString.split(/[,\//]/).filter(value => value.trim() !== '')
-  const allNumbers = values.every(value => !isNaN(parseFloat(value)))
-  if (allNumbers) {
-    return values.map(value => parseFloat(value))
-  }
+  const values = valueString.split(/[,\/]+/).filter(value => value.trim() !== '')
   const parsedValues: (string | Date)[] = []
-  const index = 0
-  if (index < values.length) {
-    const startTimeString = values[index]
-    const endTimeString = values[index + 1]
-    if (isValidDateString(startTimeString) && isValidDateString(endTimeString)) {
-      const startDate = parseISO(startTimeString)
-      const endDate = parseISO(endTimeString)
-      let currentDate = startDate
-      while (isBefore(currentDate, endDate) || currentDate.getTime() === endDate.getTime()) {
-        parsedValues.push(format(currentDate, "yyyy-MM-dd"))
-        currentDate = add(currentDate, { days: 1 })
+  if (isValidDateString(values[0]) && isValidDateString(values[1])) {
+    if (!isAfter(values[0], values[1])) {
+      const startDate = new Date(values[0])
+      const endDate = new Date(values[1])
+      const parsedDuration = duration.toDays(values[2])
+      let currentDate: Date = new Date(startDate)
+
+      while (currentDate <= endDate) {
+        parsedValues.push(format(new Date(currentDate), "yyy-MM-dd"))
+        currentDate = add(currentDate, { days: parsedDuration })
       }
     }
+  } else {
+    parsedValues.push(...values)
   }
   return parsedValues
 }
