58545caafee1d6b25b943ac5557af01d1620e3c1
stanleyfestus <festusstanley@gmail.com>
2024-05-16 07:51:55 +0000

    Refactored helper function

--- src/processing/util/helpers.ts
+++ src/processing/util/helpers.ts
@@ -2,41 +2,28 @@ import { add } from "date-fns"
 import * as duration from "duration-fns"
 
 export function convertDimensionStringValue (stringValue: string) {
-  const initSplit = stringValue.split(/[,]/).map(value => value.trim())
-  let seperator = false
-
-  for (const value of initSplit) {
-    if (value.includes("/")) {
-      seperator = true
-      break
-    }
-  }
-
-  return {
-    seperator,
-    value: initSplit
-  }
+  return stringValue.split('/')
 }
 
-export function parseDimensionValues (valueString: string) {
-  const values = convertDimensionStringValue(valueString)
+export function parseDimensionValues (dimensionValues: string) {
+  const initSplit = dimensionValues.split(/,/).map(value => value.trim())
+
   const parsedValues: string[] = []
-  if (values.seperator) {
-    const items = values.value[0]
-    const durationValue = values.value[0].split("/")[2]
-    const parsedDuration = duration.parse(durationValue)
-
-    const startDate = new Date(items.split('/')[0])
-    const endDate = new Date(items.split('/')[1])
-    let currentDate: Date = new Date(startDate)
-
-    while (currentDate <= endDate) {
-      parsedValues.push(new Date(currentDate).toISOString())
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      currentDate = add(currentDate, parsedDuration)
+  for (const value of initSplit) {
+    if (value.includes("/")) {
+      const valuesWithSlash = convertDimensionStringValue(value)
+      const startDate = new Date(valuesWithSlash[0])
+      const endDate = new Date(valuesWithSlash[1])
+      const parsedDuration = duration.parse(valuesWithSlash[2])
+      let currentDate = new Date(startDate)
+      while (currentDate <= endDate) {
+        parsedValues.push(new Date(currentDate).toISOString())
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        currentDate = add(currentDate, parsedDuration)
+      }
+    } else {
+      parsedValues.push(value)
     }
-  } else {
-    values.value.forEach(val => parsedValues.push(val))
   }
   return parsedValues
 }
