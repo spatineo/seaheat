2c724aa84248d82cdf2d2295666763ceceb0b5c7
Sampo Savolainen <sampo.savolainen@spatineo.com>
2024-05-17 06:13:33 +0000

    fix: make also CDK work with eslint

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { StaticResourcesS3Stack } from '../lib/static-resources-stack'
 import { CertificateStack } from '../lib/certificate-stack'
