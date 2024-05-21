fbace93538cceb1988082c7d6546f233a771b2a4
stanleyfestus <festusstanley@gmail.com>
2024-05-21 13:36:50 +0000

    test middleware wip

--- src/middleware/mathMiddleware.spec.ts
+++ src/middleware/mathMiddleware.spec.ts
@@ -1,20 +1,30 @@
 import { expect, test } from "vitest"
 import { createListenerMiddleware, isAnyOf, createAction } from '@reduxjs/toolkit'
-import { RootState, AppDispatch } from "../store"
+import { restoreDataState } from "../app/slices/data"
+import { setLocation as setFacilityLocation } from "../app/slices/facility"
+
+import intakeReducer, { setLocation as setIntakeLocation } from "../app/slices/intake"
+import { RootState, AppDispatch, store } from "../store"
 
 export const mathMiddleware = createListenerMiddleware()
 const startAppListening = mathMiddleware.startListening.withTypes<RootState, AppDispatch>()
 export const initMathAction = createAction('INIT_MATH')
 
 test('math middleware test', () => {
-  let listerStarted = false
+  const initialState = {
+    intake: { location: { lat: 3017952.0227570194, lng: 9302118.812707355 } }
+  }
+
   let state
   startAppListening({
-    matcher: isAnyOf(),
-    effect: async (action, listenerApi) => {
-      listerStarted = true
+    matcher: isAnyOf(initMathAction, setIntakeLocation, restoreDataState, setFacilityLocation),
+    effect: async (_action, listenerApi) => {
+      listenerApi.dispatch(setIntakeLocation([initialState.intake.location.lat, initialState.intake.location.lng]))
+      listenerApi.getState()
       state = listenerApi.getState()
     }
   })
-  expect(listerStarted).toBe(true)
+  expect(state).toBe({
+    intake: { location: { lat: 3017952.0227570194, lng: 9302118.812707355 } }
+  })
 })
