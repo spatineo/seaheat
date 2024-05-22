4562f4ceef1000bc7e0749dd196c8f1759f1e4ba
stanleyfestus <festusstanley@gmail.com>
2024-05-22 09:34:44 +0000

    Added second test

--- src/processing/util/calculateDifference.spec.ts
+++ src/processing/util/calculateDifference.spec.ts
@@ -10,3 +10,55 @@ test('it should return output object with empty series array', () => {
     series: []
   })
 })
+
+test("it should return calculated difference from every item from 2 arrays", () => {
+  const temperatureAtDischargeDepth: { values: number[] } | undefined = {
+    values: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+  }
+
+  const dischargeWaterTemperature = {
+    values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  }
+
+  expect(calculateDifference(temperatureAtDischargeDepth, dischargeWaterTemperature)).toEqual({
+    unit: 'C',
+    axes: {
+      x: {
+        label: 'Month',
+        values: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec"
+        ]
+      }
+    },
+    series: [
+      {
+        label: "Temperature",
+        values: [
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1
+        ]
+      }
+    ]
+  })
+})
