d3b9a67a6739aebd059120971826e17eda9f79f3
stanleyfestus <festusstanley@gmail.com>
2024-05-23 09:57:59 +0000

    deleted mathmiddlware test file

--- src/middleware/mathMiddleware.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest"
-import { acceptDistances } from '../processing/util/mathMiddlewareHelper'
-
-test('math middleware test', () => {
-  const intakeLocation = null
-  const facilityLocation = null
-  expect(acceptDistances(intakeLocation, facilityLocation)).toBeNull()
-})
