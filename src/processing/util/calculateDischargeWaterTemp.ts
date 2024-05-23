95e5faf57e59d5b2561c6ea27fbb7d385a51ca99
stanleyfestus <festusstanley@gmail.com>
2024-05-23 12:36:19 +0000

    calculateDischargeWaterTemp function

--- src/processing/util/calculateDischargeWaterTemp.ts
+++ src/processing/util/calculateDischargeWaterTemp.ts
@@ -15,7 +15,6 @@ export function calculateDischargeWaterTemp (intakeTemperature: TemperatureData,
 
   const searchForValues = findEqualDepthIndex > -1 ? intakeTemperature.temperatureValues.filter((tmp) => tmp.y === findEqualDepthIndex && tmp) : null
   if (searchForValues !== null && temperatureDelta.length > 0) {
-    console.log('temperatureDelta', temperatureDelta, searchForValues)
     Array(12).fill(0).forEach((_v, month: number) => {
       const d = new Date(2001, month, 1)
       const calculatedValues = searchForValues?.find((v) => v.x === month)?.value
