6692b641806e86bd60d2a0ea1082cbf89b3e161c
Sampo Savolainen <sampo.savolainen@spatineo.com>
2025-01-22 19:48:13 +0100

    Configure map layers and add scale line (#95)
    * muokattu layerit
    * lisätty mittakaava

--- src/config/layers.ts
+++ src/config/layers.ts
@@ -11,22 +11,8 @@ export const availableLayers: Array<LayerConfiguration> = [
     dimensions: ['time']
   },
   {
-    id: 'a530150a-0e40-4fdc-8087-d50b62404d8a',
-    title: 'Seabed_substrate_250k',
-    capabilitiesUrl: 'https://gtkdata.gtk.fi/arcgis/services/EMODnet/EMODnet_Geology/MapServer/WmsServer?SERVICE=WMS&REQUEST=GetCapabilities',
-    layer: 'Seabed_substrate_250k',
-    isDatalayer: true,
-    type: 'WMS',
-    legend: {
-      url: 'https://gtkdata.gtk.fi/arcgis/services/EMODnet/EMODnet_Geology/MapServer/WmsServer?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=Seabed_substrate_1M',
-      width: 210,
-      height: 126,
-      scale: 0.9
-    }
-  },
-  {
     id: '245305de-0385-4c3b-9447-ebe8ab100ee1',
-    title: 'Syvyysmalli / Velmu',
+    title: 'Batymetria (Velmu)',
     capabilitiesUrl: 'https://paikkatieto.ymparisto.fi/arcgis/services/Velmukartta/Velmukartta/MapServer/WmsServer?SERVICE=WMS&REQUEST=GetCapabilities',
     layer: '1',
     isDatalayer: true,
@@ -37,5 +23,37 @@ export const availableLayers: Array<LayerConfiguration> = [
       height: 468,
       scale: 0.8
     }
+  },
+  {
+    id: 'a8f3a561-e01c-49c3-8065-81c2dd22c6b2',
+    title: 'Kuntarajat',
+    capabilitiesUrl: 'https://geo.stat.fi/geoserver/tilastointialueet/wms?SERVICE=WMS&REQUEST=GetCapabilities',
+    layer: 'kunta1000k',
+    isDatalayer: false,
+    type: 'WMS'
+  },
+  {
+    id: '022ae14d-53c6-4af2-9de8-7952c27123d2',
+    title: 'Puolustusvoimien suoja-alueet',
+    capabilitiesUrl: 'https://paikkatieto.ymparisto.fi/arcgis/services/Velmukartta/Velmukartta/MapServer/WmsServer?SERVICE=WMS&REQUEST=GetCapabilities',
+    layer: '125',
+    isDatalayer: false,
+    type: 'WMS'
+  },
+  {
+    id: 'e6e6477c-05b0-4880-af84-cc0f79ba55b0',
+    title: 'Suomen aluevesiraja',
+    capabilitiesUrl: 'https://julkinen.traficom.fi/inspirepalvelu/avoin/wms?request=getcapabilities',
+    layer: 'TerritorialSeaArea_L',
+    isDatalayer: false,
+    type: 'WMS'
+  },
+  {
+    id: '4131bf07-e6a6-4411-afdd-bd000067b4d0',
+    title: 'Kalankasvatusalueet',
+    capabilitiesUrl: 'https://paikkatiedot.ymparisto.fi/geoserver/velmukartta/wms?&service=WMS&VERSION=1.3.0&REQUEST=GetCapabilities',
+    layer: 'Kalankasvattamo',
+    isDatalayer: false,
+    type: 'WMS'
   }
 ]
