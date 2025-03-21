e039f1b9d9846b6c9d6ce21f7fa6431aa7c60896
Sampo Savolainen <sampo.savolainen@spatineo.com>
2025-03-21 12:32:30 +0000

    add buttons to modify all intake volue / temperature difference values

--- src/components/monthlySlider/MonthlySlider.tsx
+++ src/components/monthlySlider/MonthlySlider.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { As, Box, HStack, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Text, Tooltip, VStack } from "@chakra-ui/react"
+import React, { useCallback } from "react"
+import { As, Box, Button, HStack, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Text, Tooltip, VStack } from "@chakra-ui/react"
 import { format } from "date-fns"
 import { MonthValue } from "../../types"
 
@@ -14,12 +14,36 @@ interface MonthlySliderProps {
   sliderIcon: As
 }
 export const MonthlySlider: React.FC<MonthlySliderProps> = ({ values, minValue, maxValue, step = 1, unit, color, changeValue, sliderIcon }: MonthlySliderProps) => {
+  const sliderWidth = 100 / (values.length + 1)
+
+  const increaseAll = useCallback(() => {
+    // Check if we can increase all values
+    if (values.findIndex(v => v >= maxValue) !== -1 || !changeValue) {
+      return
+    }
+
+    values.forEach((value, month: number) => {
+      changeValue({ month, value: value + step })
+    })
+  }, [values, maxValue, changeValue, step])
+
+  const decreaseAll = useCallback(() => {
+    // Check if we can decrease all values
+    if (values.findIndex(v => v <= minValue) !== -1 || !changeValue) {
+      return
+    }
+
+    values.forEach((value, month: number) => {
+      changeValue({ month, value: value - step })
+    })
+  }, [values, minValue, changeValue, step])
+
   return (
     <HStack width='100%'>
       {values.map((value, mon: number) => {
         const d = new Date(2001, mon, 1)
         return (
-          <VStack alignContent={'center'} width={`${100 / values.length}%`} key={mon}>
+          <VStack alignContent={'center'} width={`${sliderWidth}%`} key={mon}>
             <Text fontSize='sm' color={'#777'}>{format(d, 'LLL')}</Text>
             <Slider
               orientation='vertical'
@@ -42,6 +66,10 @@ export const MonthlySlider: React.FC<MonthlySliderProps> = ({ values, minValue, 
           </VStack>
         )
       })}
+      <VStack alignContent={'center'} width={`${sliderWidth}%`}>
+        <Button size="xs" onClick={increaseAll}>+</Button>
+        <Button size="xs" onClick={decreaseAll}>-</Button>
+      </VStack>
     </HStack>
   )
 }
